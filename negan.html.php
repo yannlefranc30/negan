42b41e1a84fa19782a63444465c23d5f94a4da98
yann <yannlefranc330@gmail.com>
2023-05-16 19:04:45 +0200

    projet negan

---
+++ negan.html.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Negan</title>
+    <link rel="stylesheet" href="image.css">
+</head>
+<body>
+<?php
+$bien = "ok";
+echo "Hello I am Negan andn you ?";
+?>
+
+<p>Trouve le php présent sur la page</p>
+
+
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
